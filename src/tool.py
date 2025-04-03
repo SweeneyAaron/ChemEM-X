6fe4e5388f3568883588691757fa6c167fe9e7b6
Aaron Sweeney <ucbtasw@ucl.ac.uk>
2025-04-03 10:39:25 +0200

    turned off debug mode

--- src/tool.py
+++ src/tool.py
@@ -149,7 +149,7 @@ class CHEMEM(HtmlToolInstance):
         
         #enable for debugging
         #TODO!
-        self.session.metadata = self
+        #self.session.metadata = self
     
 
         
